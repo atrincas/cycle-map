a3eafdb4d98a9d622ad79b3fa24368cf21792397
atrincas <atrincas@gmail.com>
2024-07-20 08:43:10 +0200

    Added failing unit tests for Pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,8 @@
+interface Props {
+  pages: number[]
+  onPageClick: (page: number) => void
+}
+
+export function Pagination(props: Props) {
+  return <div>Pagination Component</div>
+}
---
+++ src/components/Pagination/__tests__/Pagination.test.tsx
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Pagination } from '../Pagination'
+
+describe('@components/Pagination', () => {
+  let handleOnpageClick: (page: number) => void
+  const pages = [1, 2, 3]
+
+  beforeEach(() => {
+    handleOnpageClick = vi.fn()
+  })
+
+  it('should render the correct buttons', () => {
+    render(<Pagination pages={pages} onPageClick={handleOnpageClick} />)
+
+    const pageButtons = screen.getAllByRole('button', { name: /Page \d+/i })
+    expect(screen.getByRole('button', { name: 'Previous page' })).toHaveTextContent('Previous')
+    expect(screen.getByRole('button', { name: 'Next page' })).toHaveTextContent('Next')
+    expect(pageButtons).toHaveLength(pages.length)
+  })
+
+  it('should call onPageClick when a button is clicked', () => {
+    render(<Pagination pages={pages} onPageClick={handleOnpageClick} />)
+
+    const button = screen.getByRole('button', { name: 'Page 3' })
+
+    fireEvent.click(button)
+
+    expect(handleOnpageClick).toHaveBeenCalledWith(3)
+  })
+})
