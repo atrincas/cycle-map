203f2e1d1978cc922a04c6f03ad86a1d040c3045
atrincas <atrincas@gmail.com>
2024-07-21 10:17:46 +0200

    Refactor styles Pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,3 +1,5 @@
+import { ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon } from 'lucide-react'
+
 interface Props {
   pages: number[]
   currentPage: number
@@ -5,36 +7,64 @@ interface Props {
 }
 
 export function Pagination({ pages, currentPage, onPageClick }: Props) {
+  const totalPages = pages.length
+  const getPageNumbers = () => {
+    if (totalPages <= 3) {
+      return pages
+    }
+
+    if (currentPage <= 3) {
+      return [1, 2, 3]
+    }
+
+    if (currentPage === totalPages) {
+      return [currentPage - 2, currentPage - 1, currentPage]
+    }
+
+    return [currentPage - 1, currentPage, currentPage + 1]
+  }
+
   return (
     <nav aria-label="Pagination">
-      <ul>
+      <ul className="flex items-center gap-1">
         <li>
           <button
+            className="flex items-center gap-1 text-foreground text-sm font-semibold rounded py-2 px-4 hover:bg-toreabay-50"
             onClick={() => onPageClick(currentPage - 1)}
             disabled={currentPage === 1}
             aria-label="Previous page"
           >
-            Previous
+            <ChevronLeftIcon />
+            <span>Previous</span>
           </button>
         </li>
-        {pages.map((page) => (
+        {getPageNumbers().map((page) => (
           <li key={page}>
             <button
+              key={`page-number-${page}`}
+              className="page-btn"
               onClick={() => onPageClick(page)}
               aria-current={page === currentPage ? 'page' : undefined}
               aria-label={`Page ${page}`}
             >
-              {page}
+              <span>{page}</span>
             </button>
           </li>
         ))}
+        {currentPage !== totalPages && (
+          <li className="flex items-center text-foreground p-2.5">
+            <span>...</span>
+          </li>
+        )}
         <li>
           <button
+            className="flex items-center gap-1 text-foreground text-sm font-semibold rounded py-2 px-4 hover:bg-toreabay-50"
             onClick={() => onPageClick(currentPage + 1)}
-            disabled={currentPage === pages[pages.length - 1]}
+            disabled={currentPage === totalPages}
             aria-label="Next page"
           >
-            Next
+            <span>Next</span>
+            <ChevronRightIcon />
           </button>
         </li>
       </ul>
