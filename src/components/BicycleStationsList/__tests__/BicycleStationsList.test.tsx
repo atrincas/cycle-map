fca3b3a3abca2b96eda9f4c84b387e824b8fceb4
atrincas <atrincas@gmail.com>
2024-07-21 14:51:01 +0200

    Fixed broken unit tests

--- src/components/BicycleStationsList/__tests__/BicycleStationsList.test.tsx
+++ src/components/BicycleStationsList/__tests__/BicycleStationsList.test.tsx
@@ -1,12 +1,12 @@
 import mockData from '@/__mocks__/networkStations.json'
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { BicycleStationsList } from '../BicycleStationsList'
 import { STATIONS_PAGE_SIZE } from '@/lib/constants'
 
 describe('@components/BicycleStationsList', () => {
   it('should render the correct values for the headers', () => {
-    render(<BicycleStationsList data={mockData.network.stations} />)
+    render(<BicycleStationsList data={mockData.network.stations} onPaginationChange={vi.fn()} />)
 
     expect(screen.getByText('station name')).toBeDefined()
     expect(screen.getByText('free bikes')).toBeDefined()
@@ -14,7 +14,7 @@ describe('@components/BicycleStationsList', () => {
   })
 
   it('should render the correct rows', () => {
-    render(<BicycleStationsList data={mockData.network.stations} />)
+    render(<BicycleStationsList data={mockData.network.stations} onPaginationChange={vi.fn()} />)
 
     const rows = screen.getAllByRole('row')
     expect(rows).toHaveLength(STATIONS_PAGE_SIZE + 1) // +1 for header row
